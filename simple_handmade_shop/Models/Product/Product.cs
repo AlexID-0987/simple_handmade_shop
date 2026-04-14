0c5d12682895bcbd670705ebfd8c931b85f1331e
alex <a1980dominov@gmail.com>
2026-04-14 23:09:47 +0300

    Added another property

--- simple_handmade_shop/Models/Product/Product.cs
+++ simple_handmade_shop/Models/Product/Product.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public int Quantity { get; set; }
         public decimal Price { get; set; }
     }
 }
