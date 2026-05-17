cc7161cce4b4ed079acdd3b0cb3925a2ec4b9143
alex <a1980dominov@gmail.com>
2026-05-17 21:47:35 +0300

    I`m adding new function in tests project

--- simple_handmade_shop.tests/UnitTest1.cs
+++ simple_handmade_shop.tests/UnitTest1.cs
@@ -6,12 +6,13 @@ namespace simple_handmade_shop.tests
 {
     public class UnitTest1
     {
+        Mock<IHelperAdmin> mock = new Mock<IHelperAdmin>();
+        Mock<IDashboard> mockDashboard = new Mock<IDashboard>();
         [Fact]
         public void Product_Creation_Valid_Redirect()
         {
-            Mock<IHelperAdmin> mock = new Mock<IHelperAdmin>();
-            Mock<IDashboard> mockDashboard = new Mock<IDashboard>();    
-            var controller = new Controllers.AdminController(mock.Object,mockDashboard.Object );
+
+            var controller = new Controllers.AdminController(mock.Object, mockDashboard.Object);
             Product product = new Product()
             {
                 Id = 1,
@@ -23,7 +24,43 @@ namespace simple_handmade_shop.tests
             var result = controller.Create(product);
             Assert.IsType<Microsoft.AspNetCore.Mvc.RedirectToActionResult>(result);
             mock.Verify(m => m.AddProductList(It.IsAny<Product>()), Times.Once);
-            
+
+        }
+        [Fact]
+        public void Admin_delete_product_valid_redirect()
+        {
+            var controller = new Controllers.AdminController(mock.Object, mockDashboard.Object);
+            int productId = 1;
+            var result = controller.Delete(productId);
+            Assert.IsType<Microsoft.AspNetCore.Mvc.RedirectToActionResult>(result);
+            mock.Verify(m => m.RemoveProductList(productId), Times.Once);
+        }
+        [Fact]
+        public async Task Admin_Edit_Product_Valid_Redirect()
+        {
+            var controller = new Controllers.AdminController(mock.Object, mockDashboard.Object);
+            int productId = 1;
+            Product product = new Product()
+            {
+                Id = 1,
+                Name = "Test Product",
+                Description = "This is a test product.",
+                Price = 9.99m,
+                FullDescription = "This is a full description of the test product."
+            };
+            mock.Setup(m => m.EditProduct(productId)).ReturnsAsync(product);
+            var result = await controller.Edit(productId);
+            Assert.IsType<Microsoft.AspNetCore.Mvc.ViewResult>(result);
+            var viewResult = result as Microsoft.AspNetCore.Mvc.ViewResult;
+            Assert.NotNull(viewResult);
+            Assert.Equal(product, viewResult.Model);
+        }
+        [Fact]
+        public void Admin_return_view_create_product()
+        {
+            var controller = new Controllers.AdminController(mock.Object, mockDashboard.Object);
+            var result = controller.Create();
+            Assert.IsType<Microsoft.AspNetCore.Mvc.ViewResult>(result);
         }
     }
 }
